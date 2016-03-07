ffc8b0333c4e832976a35c85a11992e2b327f34c
InfinityRaider <InfinityRaider@users.noreply.github.com>
2016-03-07 23:51:17 +0100

    Settler inventory gui

--- src/main/java/com/InfinityRaider/settlercraft/settlement/settler/container/ContainerSettlerInventory.java
+++ src/main/java/com/InfinityRaider/settlercraft/settlement/settler/container/ContainerSettlerInventory.java
@@ -6,8 +6,11 @@ import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
 
 public class ContainerSettlerInventory extends ContainerSettler {
-    public static final int INVENTORY_X = 10;
-    public static final int INVENTORY_Y = 10;
+    public static final int INVENTORY_SETTLER_X = 84;
+    public static final int INVENTORY_SETTLER_Y = 8;
+
+    public static final int INVENTORY_PLAYER_X = 84;
+    public static final int INVENTORY_PLAYER_Y = 98;
 
     private final InventoryPlayer playerInventory;
     private final IInventorySettler settlerInventory;
@@ -20,8 +23,8 @@ public class ContainerSettlerInventory extends ContainerSettler {
     }
 
     private void addSlotsToContainer() {
-        int xOffset = INVENTORY_X;
-        int yOffset = INVENTORY_Y;
+        int xOffset = INVENTORY_SETTLER_X;
+        int yOffset = INVENTORY_SETTLER_Y;
         // add settler inventory to the container
         for(int i=0;i<3;i++) {
             for(int j=0;j<9;j++) {
@@ -30,7 +33,8 @@ public class ContainerSettlerInventory extends ContainerSettler {
             }
         }
         //add player's inventory to the container
-        yOffset = yOffset + 3 * 18;
+        xOffset = INVENTORY_PLAYER_X;
+        yOffset = INVENTORY_PLAYER_Y;
         for(int i=0;i<3;i++) {
             for(int j=0;j<9;j++) {
                 //new Slot(inventory, slot index, x coordinate, y coordinate)
